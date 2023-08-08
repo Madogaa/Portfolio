692a5b8bf68e282b1dee91464a3212c78e18e786
“Mariodg” <mdoga2001@gmail.com>
2023-08-08 20:02:00 +0200

    Prueba Entry

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ export default function App() {
 
   return (
     <>
-    <EntryComponent />
     <AnimationComponent />
     <section className="spikes"></section>
     <section className="wavecontainer">
