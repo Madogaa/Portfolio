35963def33c77c11b22dc4664a8d0b0918d69983
“Mariodg” <mdoga2001@gmail.com>
2023-08-10 19:28:10 +0200

    Delete Prevent Default ContactForm

--- src/Contact/ContactForm.jsx
+++ src/Contact/ContactForm.jsx
@@ -6,8 +6,7 @@ const ContactForm = () => {
     const [subject, setSubject] = useState('');
     const [description, setDescription] = useState('');
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+    const handleSubmit = async () => {
 
       try {
         const response = await axios.post('https://backend-chi-fawn.vercel.app/send-email', {
